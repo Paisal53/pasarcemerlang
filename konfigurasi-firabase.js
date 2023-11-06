47ffe7814332093bb10d2cf2dad5efb68d5f17d0
alatiff045@gmail.com <alatiff045@gmail.com>
2023-11-06 09:36:55 +0700

    test

---
+++ konfigurasi-firabase.js
@@ -0,0 +1,9 @@
+export const konfigurasiFirabase = {
+apiKey: "AIzaSyBbcKf-6vqlCNMptp2Eq5NxbCVGVnZkjMM",
+  authDomain: "pasaronline-5bb7c.firebaseapp.com",
+  projectId: "pasaronline-5bb7c",
+  storageBucket: "pasaronline-5bb7c.appspot.com",
+  messagingSenderId: "189226313387",
+  appId: "1:189226313387:web:1fb7848af1e98cd947b9ee",
+  measurementId: "G-RMVMKZVKMK"
+};
